dffa33eefba99fe778517c11ed38793ae74bbf42
warzes <warzes@mail.ru>
2024-12-23 14:40:21 +1000

    - файлы проекта

---
+++ src/Engine/stdafx.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#define _ENGINE_PRIVATE_HEADER
+
+
+#if defined(_MSC_VER)
+#	pragma warning(disable : 4514)
+#	pragma warning(disable : 4623)
+#	pragma warning(disable : 4625)
+#	pragma warning(disable : 4626)
+#	pragma warning(disable : 4820)
+#	pragma warning(disable : 5045)
+#endif
---
+++ src/ref.md
@@ -0,0 +1,3 @@
+﻿std::expected - вариант optional, позволяющий вернуть код ошибки или объект
+	надо юзать
+	https://habr.com/ru/articles/737408/
